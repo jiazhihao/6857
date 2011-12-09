78a94f7731e38a50c86fecffa7f0e2392579d0d9
Xi Wang <xi@csail.mit.edu>
2011-12-09 03:30:57 -0500

    x

--- code/bridge-p.c
+++ code/bridge-p.c
@@ -2,7 +2,7 @@ maxnum0 = ...; /* read from userspace */
 if (maxnum0 > PAGE_SIZE / sizeof(struct __fdb_entry))
     maxnum1 = PAGE_SIZE / sizeof(struct __fdb_entry);
 else
-    maxnum1 = maxnum;
+    maxnum1 = maxnum0;
 size = maxnum1 * sizeof(struct __fdb_entry);
 buf = kmalloc(size, GFP_USER);
 ...
