cda2a854f80f28b8a39ec6b7f6d9ed1fdcbba459
Xi Wang <xi@csail.mit.edu>
2011-12-24 22:41:06 -0500

    allow equations in c comments

--- code/ax25-sign.c
+++ code/ax25-sign.c
@@ -4,10 +4,10 @@ static int ax25_setsockopt(...,
 {
 	char devname[IFNAMSIZ];
 	...
-	/* optlen will be silently converted to unsigned */
+	/* optlen is treated as unsigned: $2^{32}-1$ */
 	if (optlen < sizeof(int))
 		return -EINVAL;
-	/* optlen is treated as signed here */
+	/* optlen is treated as signed: $-1$ */
 	if (optlen > IFNAMSIZ)
 		optlen = IFNAMSIZ;
 	copy_from_user(devname, optval, optlen);
