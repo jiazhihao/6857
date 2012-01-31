fadc4ba0955db7003874382c086876f6e267ee55
Xi Wang <xi@csail.mit.edu>
2012-01-31 14:28:22 -0500

    reorg eval

--- code/ax25-sign.c
+++ code/ax25-sign.c
@@ -4,7 +4,7 @@ static int ax25_setsockopt(...,
 {
 	char devname[IFNAMSIZ];
 	...
-	/* assume optlen = 0xffffffff */
+	/* consider optlen = 0xffffffff */
 	/* optlen is treated as unsigned: $2^{32}-1$ */
 	if (optlen < sizeof(int))
 		return -EINVAL;
--- code/sctp-bad.c
+++ code/sctp-bad.c
@@ -1,4 +1,5 @@
-if ((INT_MAX - key_len) < sizeof(struct sctp_auth_bytes))
+// __u32 key_len
+if (INT_MAX - key_len < sizeof(struct sctp_auth_bytes))
 	return NULL;
 key = kmalloc(sizeof(struct sctp_auth_bytes)
 				+ key_len, gfp);
