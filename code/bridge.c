ea2bd8fbb2293e7d33e13a1ad8887c948ec205ab
Nickolai Zeldovich <nickolai@csail.mit.edu>
2012-04-18 19:40:18 -0400

    otherwise, no harmful overflow occurs in figure 1

--- code/bridge.c
+++ code/bridge.c
@@ -9,7 +9,7 @@ if (size > PAGE_SIZE) {
 	/* limit to 256 = 4096 / 16 */
 	maxnum = PAGE_SIZE / sizeof(struct __fdb_entry);
 }
-buf = kmalloc(size, GFP_USER);
+struct __fdb_entry *buf = kmalloc(size, GFP_USER);
 if (!buf)
 	return -ENOMEM;
 /* write to buf[0..maxnum-1] */
