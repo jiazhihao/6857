58b44f5db661bfaf3231e4604f484709e9eb04ac
Xi Wang <xi.wang@gmail.com>
2012-05-01 22:15:39 -0400

    kill empty lines

--- code/ext4-bad.c
+++ code/ext4-bad.c
@@ -1,11 +1,9 @@
-
-	sbi->s_log_groups_per_flex = ...; /* from disk */
-	groups_per_flex = 1 << sbi->s_log_groups_per_flex;
-
-	/* patched check */
-	if (groups_per_flex < 2) {
-		sbi->s_log_groups_per_flex = 0;
-		return 1;
-	}
-
-	flex_group_count = ... / groups_per_flex;
+sbi->s_log_groups_per_flex = ...; /* from disk */
+groups_per_flex = 1 << sbi->s_log_groups_per_flex;
+/* patched check */
+if (groups_per_flex < 2) {
+	sbi->s_log_groups_per_flex = 0;
+	return 1;
+}
+/* groups_per_flex is used as a divisor */
+flex_group_count = ... / groups_per_flex;
--- code/oom.c
+++ code/oom.c
@@ -1,7 +1,5 @@
 	long points; /* int points; */
-	...
 	points = get_mm_rss(p->mm) + p->mm->nr_ptes;
 	points += get_mm_counter(p->mm, MM_SWAPENTS);
-
 	points *= 1000;
 	points /= totalpages;
