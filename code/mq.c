df7b6a12751f2f8323c58e26621f8e3f837d28f7
Xi Wang <xi.wang@gmail.com>
2012-05-03 14:43:55 -0400

    nit

--- code/mq.c
+++ code/mq.c
@@ -1,8 +1,8 @@
 /* overflow checks for
  *   maxmsg * msgsize + maxmsg * sizeof(void *)
  * assume
- *   $\cc{maxmsg} > 0 \land \cc{maxmsg} \leq 2^{15}$
- *   $\cc{msgsize} > 0 \land \cc{msgsize} \leq 2^{20}$
+ *   0 < maxmsg $\leq$ $2^{15}$
+ *   0 < msgsize $\leq$ $2^{31}$
  */
 if (msgsize > ULONG_MAX / maxmsg)
 	return 0;
