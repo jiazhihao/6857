b4a06db3debc20128456d20a1307cd813eada01d
Zhihao Jia <dugushuiyi@gmail.com>
2012-05-03 14:47:33 -0400

    make it more precise

--- code/mq.c
+++ code/mq.c
@@ -2,7 +2,7 @@
  *   maxmsg * msgsize + maxmsg * sizeof(void *)
  * assume
  *   0 < maxmsg $\leq$ $2^{15}$
- *   0 < msgsize $\leq$ $2^{31}$
+ *   0 < msgsize $<$ $2^{31}$
  */
 if (msgsize > ULONG_MAX / maxmsg)
 	return 0;
