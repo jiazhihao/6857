27a12200ff03dceeb52ac4056d3a1e021379efda
Xi Wang <xi.wang@gmail.com>
2012-05-03 13:05:24 -0400

    add ->read_status() impl

--- code/olpc-sign.c
+++ code/olpc-sign.c
@@ -2,7 +2,18 @@ struct dcon_platform_data {
 	...
 	u8 (*read_status)(void);
 };
-struct dcon_platform_data *pdata = ...;
-int status = pdata->read_status();
-if (status == -1)
-	return IRQ_NONE;
+/* ->read_status() implementation */
+static u8 dcon_read_status_xo_1_5(u8 *status)
+{
+	if (!dcon_was_irq())
+		return -1;
+	...
+}
+irqreturn_t dcon_interrupt(int irq, void *id)
+{
+	struct dcon_platform_data *pdata = ...;
+	int status = pdata->read_status();
+	if (status == -1)
+		return IRQ_NONE;
+	...
+}
